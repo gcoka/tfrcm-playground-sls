3684069147065becf00c9d362573eea506d4486b
Takuya Fujimoto <t.fujimoto@gemcook.com>
2018-08-26 16:05:14 +0900

    [fix] ### lambda function return promise

--- node/src/handlers/edge.js
+++ node/src/handlers/edge.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports.hello = async (event, context) => {
-  console.log(event);
+  console.log(JSON.stringify(event));
+
+  const request = event.Records[0].cf.request;
+
+  try {
+    if (request.uri !== '/index.html') {
+      console.log(`changing ${request.uri} to rewrite.html`);
+      request.uri = '/rewrite.html';
+    }
+
+    return request;
+  } catch (error) {
+    return error;
+  }
 };
